b2095cd5ff653878077fd13cc8b010c7808f3098
unclebill <billbill290@gmail.com>
2022-03-14 17:20:21 +0800

    feat: introduce SocialNetwork.Shared#utils.share as succession of getShareLinkURL
    closes #MF-194

--- packages/mask/src/social-network-adaptor/twitter.com/shared.ts
+++ packages/mask/src/social-network-adaptor/twitter.com/shared.ts
@@ -1,7 +1,9 @@
 import { PostIdentifier, ProfileIdentifier } from '@masknet/shared-base'
+import urlcat from 'urlcat'
 import type { SocialNetwork } from '../../social-network/types'
 import { createSNSAdaptorSpecializedPostContext } from '../../social-network/utils/create-post-context'
 import { deconstructPayload } from '../../utils'
+import { createCenterWindow } from '../utils'
 import { twitterBase } from './base'
 import { twitterEncoding } from './encoding'
 import { usernameValidator } from './utils/user'
@@ -33,9 +35,17 @@ export const twitterShared: SocialNetwork.Shared & SocialNetwork.Base = {
             },
         },
         getPostURL,
-        getShareLinkURL(message) {
-            return new URL(`https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`)
+        share(text) {
+            const win = createCenterWindow(350, 600)
+            const url = urlcat('https://twitter.com/intent/tweet', { text })
+            if (win) {
+                win.location.assign(url)
+                win.document.addEventListener('DOMContentLoaded', win.focus)
+            }
         },
+        // getShareLinkURL(message) {
+        //     return new URL(`https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`)
+        // },
         createPostContext: createSNSAdaptorSpecializedPostContext({
             payloadParser: deconstructPayload,
             payloadDecoder: twitterEncoding.payloadDecoder,
